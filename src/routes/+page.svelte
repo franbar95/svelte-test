f8e9c09bdea1c6178a000b6d3c81ebeec0b49a80
franbar95 <franbar1094@gmail.com>
2025-08-07 17:35:53 -0300

    cambio 1

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,4 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
-<h2>test svelte para vercel
-</h2>
+<h2>test svelte para vercel</h2>
+<h3>texto h3</h3>
